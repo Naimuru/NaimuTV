10089108d1218d7975e20b433155c63cb1772e3d
naimuru <nbello@unal.edu.co>
2023-03-23 01:22:48 -0500

    Updated

--- server.py
+++ server.py
@@ -9,12 +9,13 @@ app = Flask(__name__)
 def turn_off_monitor():
     system = platform.system()
     if system == "Windows":
-        subprocess.call("powershell.exe -command (Add-Type '[DllImport(\"user32.dll\")] public static extern int SendMessage(IntPtr hWnd, UInt32 Msg, UIntPtr wParam, IntPtr lParam);' -Name a -Pas)::SendMessage([IntPtr]::Zero, 0x0112, [UIntPtr]::op_Explicit(0xF170), [IntPtr]::op_Explicit(2))")
+        subprocess.call('powershell.exe -command "Add-Type -TypeDefinition \'public class User32 { [System.Runtime.InteropServices.DllImport(\\"user32.dll\\")] public static extern int SendMessage(int hWnd, uint Msg, int wParam, int lParam); }\' -PassThru; $WM_SYSCOMMAND = 0x0112; $SC_MONITORPOWER = 0xF170; $MonitorOff = 2; [User32]::SendMessage(-1, $WM_SYSCOMMAND, $SC_MONITORPOWER, $MonitorOff)"', shell=True)
     elif system == "Linux":
         subprocess.call("xset dpms force off", shell=True)
     else:
         return "Unsupported operating system", 500
     return "Monitor turned off", 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
