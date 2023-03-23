a4c9a413d1b0effa2cc1c5fe7bdca3b2a99e582c
naimuru <nbello@unal.edu.co>
2023-03-23 02:27:22 -0500

    Initial Commit

---
+++ NaimuTV/NaimuTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaimuTVApp.swift
+//  NaimuTV
+//
+//  Created by Naimu on 23/03/23.
+//
+
+import SwiftUI
+
+@main
+struct NaimuTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
