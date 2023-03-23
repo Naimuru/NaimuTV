dd2eabd3c30a07e80706fd6eef6ac6c28e60a267
naimuru <nbello@unal.edu.co>
2023-03-23 01:16:29 -0500

    Upgraded to work in a 64-bit windows machine

--- server.py
+++ server.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def turn_off_monitor():
     system = platform.system()
     if system == "Windows":
-        subprocess.call("powershell.exe -command (Add-Type '[DllImport(\"user32.dll\")] public static extern int SendMessage(int hWnd, int hMsg, int wParam, int lParam);' -Name a -Pas)::SendMessage(-1,0x0112,0xF170,2)")
+        subprocess.call("powershell.exe -command (Add-Type '[DllImport(\"user32.dll\")] public static extern int SendMessage(IntPtr hWnd, UInt32 Msg, UIntPtr wParam, IntPtr lParam);' -Name a -Pas)::SendMessage([IntPtr]::Zero, 0x0112, [UIntPtr]::op_Explicit(0xF170), [IntPtr]::op_Explicit(2))")
     elif system == "Linux":
         subprocess.call("xset dpms force off", shell=True)
     else:
@@ -17,4 +17,4 @@ def turn_off_monitor():
     return "Monitor turned off", 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=5000)
